9b97834fd7b0c9b64e6242570bcc47814657981b
Yadu Krishnan <yadavan88@gmail.com>
2022-04-28 14:03:55 +0200

    Added code samples for part 2 of the blog series

---
+++ src/main/scala/com/yadavan88/ce/part2/IOMethods.scala
@@ -0,0 +1,50 @@
+package com.yadavan88.ce.part2
+
+import cats.effect.IOApp
+import cats.effect.IO
+
+object IOMethods extends IOApp.Simple {
+
+  val io1 = IO("Scala")
+  val io2 = IO("Cats")
+
+  val mapPgm: IO[String] = io1.map(_.toUpperCase)
+  val flatMapPgm: IO[String] = io1.flatMap(a => io2.map(a + _))
+  val forComp: IO[String] = for {
+    a <- io1
+    b <- io2
+  } yield a + b
+
+  val strIO: IO[String] = IO("Cats Effect")
+  val voidIO: IO[Unit] = strIO.void
+  val asIntIO: IO[Int] = strIO as 100
+
+  val printIO = IO.println("Hello World")
+  val deferredIO: IO[String] = IO.defer(IO("String"))
+
+  val aIO: IO[String] = IO("Hello")
+  val anotherIO: IO[String] = IO(aIO).flatten
+
+  val aFailedIO: IO[String] = IO.raiseError[String](new Exception("Failed IO"))
+
+  val aFailedIntIO = IO.raiseError[Int](new Exception("Not a valid number"))
+  val handledIO: IO[Int] = aFailedIntIO.handleError(ex => 0)
+  val handledWithIO: IO[Int] = aFailedIntIO.handleErrorWith(_ => IO.pure(0))
+
+  val intIO: IO[Int] = IO(100)
+  val redeemedIO: IO[String] = intIO.redeem(_ => "failed", _ => "success")
+
+  val num = scala.util.Random.nextInt(4)
+  val raisedIO: IO[Unit] =
+    IO.raiseWhen(num == 0)(new RuntimeException("Number can not be 0"))
+
+  val firstIO: IO[Int] = IO(100)
+  val secondIO: IO[String] = IO("Millions")
+
+  val firstSecond: IO[String] = firstIO *> secondIO
+  val secondFirst: IO[Int] = firstIO <* secondIO
+
+  val anotherCombinator: IO[String] = firstIO >> secondIO
+
+  override def run: IO[Unit] = redeemedIO.map(println)
+}
